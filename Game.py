19a7e91b6b3b41a527941800af220ad306c80c9e
thezachwooten <zbwoote@clemson.edu>
2024-10-16 18:07:27 -0400

    Renamed folder and added utils.py

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from Data.Scripts.Background import * # import code for parallax effect
+from Data.Scripts.Entities import *
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -16,28 +17,41 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.movement = [False, False]
+
         # Initialize the background
         self.background = Background('Castle', self.screen, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        # Initialize player
+        self.player = PhysicsEntitiy(self, 'player', (0,self.background.ground_height), (8,15))
     
 
     def run(self):
         # Main Game Loop
         while True:
+            self.player.update((self.movement[1] - self.movement[0], 0))
+           
+            # Scroll control based on player input
+            key = pygame.key.get_pressed()
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if key[pygame.K_LEFT]:
+                        self.background.update_scroll('left')
+                    if key[pygame.K_RIGHT]:
+                        self.background.update_scroll('right')
+
 
             # Draw the background and ground
             self.background.draw_bg()
             self.background.draw_ground()
 
-            # Scroll control based on player input
-            key = pygame.key.get_pressed()
-            if key[pygame.K_LEFT]:
-                self.background.update_scroll('left')
-            if key[pygame.K_RIGHT]:
-                self.background.update_scroll('right')
+            
+            
 
             # update 
             pygame.display.update()
