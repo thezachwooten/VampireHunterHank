c6ab21ec58c484f1a0a8cb5ec25da0980dbd2138
thezachwooten <zbwoote@clemson.edu>
2024-10-17 14:37:28 -0400

    Added new assets

--- Data/Scripts/tilemap.py
+++ Data/Scripts/tilemap.py
@@ -1,10 +1,4 @@
 import pygame
 
 class Tilemap:
-    def __init__(self, tile_size=16):
-        self.tile_size = tile_size
-        self.tilemap = {} # ON SCREEN
-        self.offgrid_tiles = [] # OFF SCREEN
-
-        for i in range(10):
-            self.tilemap[str(3 + i) + ';10'] = {'typeL'}
+    pass
