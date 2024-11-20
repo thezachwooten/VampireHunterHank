679793660e8d6f6d8db5a93e525a2f9805d05367
thezachwooten <zbwoote@clemson.edu>
2024-11-20 12:49:20 -0500

    Updated Skeleton
    Removed pointless variables in constructor

--- Data/Scripts/Skeleton.py
+++ Data/Scripts/Skeleton.py
@@ -24,20 +24,16 @@ class Skeleton(pygame.sprite.Sprite):
         self.position, self.velocity = pygame.math.Vector2(position[0], position[1]), pygame.math.Vector2(0, 0)
         self.current_animation = self.animations['idle']  # Start with idle animation
         self.image = self.current_animation.get_current_frame()
+        # Rect alignment
         self.rect = self.image.get_rect()
+        self.rect.centery = self.position.y  # Align the bottom of the rect with the starting position
+        self.rect.centerx = self.position.x  # Optionally center the rect horizontally at the position
 
         self.update_image() # update ghoul image/mask
 
         self.last_move_time = 0
         self.state = 'pause'  # Start in the "move_left" state
-        self.previous_state = 'move_left'
         self.FACING_LEFT = random.choice([True, False])
-        self.MOVE_LEFT = False
-        self.MOVE_RIGHT = False
-        self.on_ground = False
-        self.gravity, self.friction = .35, -.12
-        
-        self.acceleration = pygame.math.Vector2(0, self.gravity)
 
         self.health = 100 # Initilize ghoul health to 100
         self.isDead = False
@@ -63,3 +59,9 @@ class Skeleton(pygame.sprite.Sprite):
         # check if health reaches zero
         if self.health <= 0:
             self.kill()
+        # Update the current animation
+        self.current_animation.update(dt)
+        self.image = self.current_animation.get_current_frame()
+        if self.FACING_LEFT == True:
+            self.image = pygame.transform.flip(self.image,1,0)
+        self.update_image()
